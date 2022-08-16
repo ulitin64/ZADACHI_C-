2119b5cdd2ec60f1f1b67e9f52d2a7aea60b7696
Alexandr <Shurik251083@gmail.com>
2022-08-16 12:06:26 +0400

    Задача которая принимает на вход три числа и выдаёт максимальное из этих чисел.

---
+++ ZADACHA4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите число One: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Two: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Tree: ");
+int numberTree = Convert.ToInt32(Console.ReadLine());
+
+int Max= numberOne > numberTwo && numberOne > numberTwo
+? numberOne: numberTwo > numberOne && numberTwo > numberTree
+? numberTwo: numberTree;
+
+Console.WriteLine(Max);
+
