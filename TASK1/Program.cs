31e622c17684daeb9629a7f81548f70e69a85c90
Alexandr <Shurik251083@gmail.com>
2022-08-16 11:57:31 +0400

    Задача которая на вход принимает число и выдает его квадрат (число умноженное на само себя)

---
+++ TASK1/Program.cs
@@ -0,0 +1,15 @@
+﻿ 
+/*Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+int userNumber = new int();
+Console.WriteLine("Введите число");
+userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = (userNumber * userNumber);
+
+Console.WriteLine($"Степень числа {userNumber} = {result} ");
