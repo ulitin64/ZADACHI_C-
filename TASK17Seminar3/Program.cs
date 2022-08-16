bc0f69412e8c39421f7055868f493e70f79a8fc1
Alexandr <Shurik251083@gmail.com>
2022-08-16 14:05:36 +0400

    Задача, которая принимает координаты Х и Y и выдает номер четверти плоскости в которой они находятся

---
+++ TASK17Seminar3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+17.
+Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+в которой находится эта точка.
+*/
+
+
+int getQuoterFromCoordinate(int x, int y)
+{
+int result = 0;
+if(x > 0 && y > 0)
+{
+    result = 1;
+}
+else if (x < 0 && y > 0)
+{
+result = 2;
+}
+else if (x < 0 && y < 0)
+{
+    result = 3;
+}
+else if (x > 0 && y < 0)
+{
+   result = 4; 
+}
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"X и Y не должны быть равны 0 вы вели {x} {y}");
+        Console.ResetColor();
+    }
+    return result;
+}
+int UserX = 0;
+int UserY = 0;
+
+Console.WriteLine("Введите Х: ");
+UserX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+UserY = Convert.ToInt32(Console.ReadLine());
+int quoter = getQuoterFromCoordinate(UserX,UserY);
+Console.WriteLine($"Для координаты {UserX}:{UserY} находятся в {quoter} четверти");
