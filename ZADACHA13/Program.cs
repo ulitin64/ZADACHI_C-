ed0748b4c5303f299e89679e82da80b269b89017
Alexandr <Shurik251083@gmail.com>
2022-08-16 12:10:45 +0400

    Задача которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ ZADACHA13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(number)-2;
+Console.WriteLine(k < 0 ? "В данном числе нет третьей цифры " : (number % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
