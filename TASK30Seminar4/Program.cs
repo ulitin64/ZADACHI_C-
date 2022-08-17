fd8a8b57bcc4620c27db810b9cb44d7387645af3
Alexandr <Shurik251083@gmail.com>
2022-08-17 16:34:21 +0400

    Задача которая выводит массив из 8 элементов заполненный рандомно 0 и 1

---
+++ TASK30Seminar4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int[] arr = new int[8];
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        //Создание объекта для генерации чисел
+        Random rnd = new Random();
+
+        //Получить случайное число (в диапазоне от 0 до 10)
+        int value = rnd.Next(0, 2);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+RandomArray(arr);
