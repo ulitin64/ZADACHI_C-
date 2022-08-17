4d91c94a0770baa63da8736c0c2d9c5e647a770a
Alexandr <Shurik251083@gmail.com>
2022-08-17 15:19:00 +0400

    Задача которая принимает на вход число N и выдаёт произведение чисел от 1 до N.

---
+++ TASK28Seminar4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+Console.WriteLine("Введите число N: ");
+int numberN = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int getTheMultiply(int numberN)
+{
+    int result = 1;
+    for (int i = 1; i <= numberN; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {numberN} = {getTheMultiply(numberN)}");
