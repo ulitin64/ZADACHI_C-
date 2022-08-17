74c16a6df590dc5d08441bc102fb8ef65ddc4fcd
Alexandr <Shurik251083@gmail.com>
2022-08-17 15:33:08 +0400

    Пример 2 решения TASK26Seminar4

---
+++ TASK26-2Seminar4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 
+*/
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int Counter(int num)
+{
+int count = 0;
+while(count < num)
+{
+    num = num / 10;
+    count++;
+}
+return count;
+}
+Console.WriteLine(Counter(num));
