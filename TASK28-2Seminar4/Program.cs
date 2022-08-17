2665648078d33cfa62b32df2241409419b24429b
Alexandr <Shurik251083@gmail.com>
2022-08-17 15:52:12 +0400

    Вариант 2 решения задачи TASK28Seminar4

---
+++ TASK28-2Seminar4/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine());
+
+int factorial(int number)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+Console.WriteLine($"Факториал {factorial(num)}");
