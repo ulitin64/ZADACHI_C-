c6bf584ed4569ab0150adb948e268033627935d8
Alexandr <Shurik251083@gmail.com>
2022-08-17 14:20:21 +0400

    Задача которая принимает на вход число и выдаёт количество цифр в числе.

---
+++ TASK26Seminar4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int howMuchIsTheDigits(int number)
+{
+    int Counter = 0;
+    while (number % 10 != 0)
+    {
+        number = number / 10;
+        Counter++;
+    }
+    return Counter;
+}
+
+Console.WriteLine($"Количетсво цифр в числе {number} = {howMuchIsTheDigits(number)}");
