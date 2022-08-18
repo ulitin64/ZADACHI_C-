98d1dce7921a062ae8fd77e69ce8ef50e49beceb
Alexandr <Shurik251083@gmail.com>
2022-08-18 10:40:22 +0400

    Задача выводит сумму всех цифр в числе

---
+++ ZADACHA27/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNumber(number);
+
+int SumNumber(int number)
+{
+
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+
+Console.WriteLine("Сумма всех цифр в числе: " + sumNumber);
