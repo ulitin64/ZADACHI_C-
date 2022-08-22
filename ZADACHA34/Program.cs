03d1d9af3f693a98c247f7ebfbbde97932ee5651
Alexandr <Shurik251083@gmail.com>
2022-08-22 12:09:43 +0400

    Массив заполненный случайными положительными трёхзначными числами.Показывающий кол-во четных чисел в массиве

---
+++ ZADACHA34/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+Например:
+[345, 897, 568, 234] -> 2
+*/
+
+int[] numbers = new int[4];
+
+void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int QuantityPositive(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1)
+        {
+            quantity++;
+        }
+    }
+    return quantity;
+}
+
+FillArray(numbers, 100, 999);
+WriteArray(numbers);
+Console.WriteLine();
+
+
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
