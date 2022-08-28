712e653a8542bc58d75e14dbfba84a2e64eaeaf2
Alexandr <Shurik251083@gmail.com>
2022-08-28 12:54:15 +0400

    Посчитайть, сколько чисел больше 0 ввёл пользователь.

---
+++ ZADACHA41/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write($"Введи количество чисел в массиве): ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[M];
+
+void InputNumbers(int M){
+for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(M);
+
+Console.WriteLine($"Количество введенных чисел больше 0: {Comparison(massiveNumbers)} ");
