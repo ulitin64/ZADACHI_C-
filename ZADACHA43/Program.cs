061577386028ba8ecc33831cbfd07ce6bfec71ab
Alexandr <Shurik251083@gmail.com>
2022-08-28 13:07:00 +0400

    Задача которая находит точку пересечения двух прямых

--- ZADACHA43/Program.cs
+++ ZADACHA43/Program.cs
@@ -7,6 +7,7 @@ b1 = 2, k1 = 5,
 b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
+
 double[,] coef = new double[2, 2];
 double[] crossPoint = new double[2];
 
@@ -14,11 +15,11 @@ void InputCoefficients()
 {
     for (int i = 0; i < coef.GetLength(0); i++)
     {
-        Console.Write($"Введите коэффициенты {i + 1}-го уравнения (y = k * x + b):\n");
+        Console.Write($"Введите значения для {i + 1} -го уравнения (y = k * x + b):\n");
         for (int j = 0; j < coef.GetLength(1); j++)
         {
-            if (j == 0) Console.Write($"Введите коэффициент k: ");
-            else Console.Write($"Введите коэффициент b: ");
+            if (j == 0) Console.Write($"Введите значение k: ");
+            else Console.Write($"Введите значение b: ");
             coef[i, j] = Convert.ToInt32(Console.ReadLine());
         }
     }
@@ -37,16 +38,15 @@ void OutputResponse(double[,] coef)
     {
         Console.Write($"Прямые совпадают");
     }
-    else if (coef[0, 0] == coef[1, 0] && coef[0, 1] != coef[1, 1])
+    else if (coef[0, 0] == coef[1, 0] && coef[0, 1]!= coef[1, 1])
     {
         Console.Write($"Прямые параллельны");
     }
     else
     {
         Decision(coef);
-        Console.Write($"Точка пересечения 2-х прямых: ({crossPoint[0]}, {crossPoint[1]})");
+        Console.WriteLine($"Точка пересечения 2-х прямых: ({crossPoint[0]}, {crossPoint[1]})");
     }
 }
-
 InputCoefficients();
 OutputResponse(coef);
